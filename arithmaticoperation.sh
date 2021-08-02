20f5beed06b67e170f896620b0fe359158984ef5
Daminichandrakar <damini.2201@gmail.com>
2021-08-02 21:27:42 +0530

    Added:store Arithmatic operation result in dictionary

--- arithmaticoperation.sh
+++ arithmaticoperation.sh
@@ -1,8 +1,13 @@
 #! /bin/bash -x
 echo "Enter 3 value " 
-read a b c
+read a 
+read b 
+read c
 
 result1=$(($a+$b*$c))
 result2=$(($a*$b+$c))
 result3=$(($c+$a/$b))
 result4=$(($a%$b+$c))
+
+ declare -A Dict
+ Dict=( [result1]=$result1 [result2]=$result2 [result3]=$result3 [result4]=$result4 )
