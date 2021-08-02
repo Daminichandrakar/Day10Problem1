c061d670258319e20bd6699cc22bfab22cfe5e83
Daminichandrakar <damini.2201@gmail.com>
2021-08-02 21:46:14 +0530

    Added:sort array in Ascending order

--- arithmaticoperation.sh
+++ arithmaticoperation.sh
@@ -39,3 +39,23 @@ do
 done
 done
 echo "array in descending order" ${array[*]}
+
+for((i=${#array[@]};i>0;i--))
+do
+for (( j=1;j<$i;j++ ))
+do
+      if [ ${array[$j-1]} -gt ${array[$j]} ]
+        then
+               temp=${array[$j-1]}
+                  array[j-1]=${array[$j]}
+                    array[j]=$temp
+         else
+                array[j-1]=${array[$j-1]}
+
+
+
+        fi
+done
+done
+echo "array in Ascending order" ${array[*]}
+
